948fc61b9e1d4335057145dc290a2b90a7c9a7d0
Arūnas Norvaiša <an@86.lt>
2019-03-14 13:55:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const budgetController = (function(){
+    //qqq
+})();
+
+const UIController = (function() {
+    //opapa
+})();
+
+const controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        // 1. Get the field input data;
+        // 2. Add the item to the budget controller;
+        // 3. Add the item to the UI;
+        // 4. Calculate the budget;
+        // 5. Display the budget in the UI.
+    });
+})(budgetController, UIController);
