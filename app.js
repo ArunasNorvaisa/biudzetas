71e435e75e4a3cd8e199bc4c4127135708a7863a
Arūnas Norvaiša <an@86.lt>
2019-03-14 16:49:15 +0200

    application progresses

--- app.js
+++ app.js
@@ -3,16 +3,45 @@ const budgetController = (function(){
 })();
 
 const UIController = (function() {
-    //opapa
+    const DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, //Either inc or exp (see html)
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+        getDOMStrings: function() { return DOMStrings; }
+    };
 })();
 
 const controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    const DOM = UIController.getDOMStrings();
+
+    const ctrlAddItem = function() {
         // 1. Get the field input data;
+        const input = UICtrl.getInput();
+        console.log('input:', input)
+
         // 2. Add the item to the budget controller;
         // 3. Add the item to the UI;
         // 4. Calculate the budget;
         // 5. Display the budget in the UI.
+    };
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener("keydown", function(event) {
+        if(event.key === "Enter") {
+            ctrlAddItem();
+        }
     });
 })(budgetController, UIController);
